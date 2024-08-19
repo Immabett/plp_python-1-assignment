9c2f65b99dd1ec3a804799b6dbb4cfcc7923993c
Immabett <jimmaculate20@gmail.com>
2024-08-19 14:41:17 +0300

    Add user input script

---
+++ user_input.py
@@ -0,0 +1,14 @@
+# user_input.py
+
+# Asking for user's name
+name = input("Immaculate Bett: ")
+
+# Asking for user's age
+age = input("23: ")
+
+# Asking for user's location
+location = input("Nairobi: ")
+
+# Printing a personalized message
+print(f"Hello {name}, you are {age} years old and live in {location}.")
+
